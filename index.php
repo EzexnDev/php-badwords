3bc83246c26a2e2dd1262be627b9f94e2a84766b
Gianmarco <gianmarco.cavaliere19093@gmail.com>
2020-12-22 17:55:17 +0100

    Fix

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ $censured = '***';
 $main_phrase = 'Cavolo !! mi sono scordato di pushare';
 
 echo '<p>'.'Frase non censurata: <strong>'.$main_phrase.'</strong></p>';
-
+var_dump($main_phrase);
 echo str_replace($_GET['badword'], $censured, $main_phrase);
